9d1c67a39c8ab475821b9f0b3bc1f7bafa4d5a16
Krisna31 <81416312+krisna31@users.noreply.github.com>
2023-06-15 12:07:34 +0700

    try linting

--- modules/restaurant_review_transform.py
+++ modules/restaurant_review_transform.py
@@ -1,6 +1,6 @@
 """Transform module
 """
- 
+
 import tensorflow as tf
 import tensorflow_transform as tft
 
@@ -19,10 +19,12 @@ NUMERICAL_FEATURES = [
 ]
 LABEL_KEY = "Target"
 
+
 def transformed_name(key):
     """Renaming transformed features"""
     return key + "_xf"
- 
+
+
 def convert_num_to_one_hot(label_tensor, num_labels=2):
     """
     Convert a label (0 or 1) into a one-hot vector
@@ -33,22 +35,22 @@ def convert_num_to_one_hot(label_tensor, num_labels=2):
     """
     one_hot_tensor = tf.one_hot(label_tensor, num_labels)
     return tf.reshape(one_hot_tensor, [-1, num_labels])
- 
- 
+
+
 def preprocessing_fn(inputs):
     """
     Preprocess input features into transformed features
-    
+
     Args:
         inputs: map from feature keys to raw features.
-    
+
     Return:
-        outputs: map from feature keys to transformed features.    
+        outputs: map from feature keys to transformed features.
     """
-    
+
     outputs = {}
-    
-    for key in CATEGORICAL_FEATURES:
+
+    for key in CATEGORICAL_FEATURES.items():
         dim = CATEGORICAL_FEATURES[key]
         int_value = tft.compute_and_apply_vocabulary(
             inputs[key], top_k=dim + 1
@@ -56,11 +58,10 @@ def preprocessing_fn(inputs):
         outputs[transformed_name(key)] = convert_num_to_one_hot(
             int_value, num_labels=dim + 1
         )
-    
+
     for feature in NUMERICAL_FEATURES:
         outputs[transformed_name(feature)] = tft.scale_to_0_1(inputs[feature])
-    
-    
+
     outputs[transformed_name(LABEL_KEY)] = tf.cast(inputs[LABEL_KEY], tf.int64)
-    
+
     return outputs
